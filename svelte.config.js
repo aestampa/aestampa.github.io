e4afad3f3aad6d9ed031d6fd71c340f852900a72
aestampa <abigail.estampador@outlook.com>
2024-06-07 19:22:09 -0600

    .

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,7 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    files: {
-      appTemplate: 'index.html' // Add this line
-    },
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-    }
+    appDir: 'app', // Required as the default is _app
   },
 };
 
