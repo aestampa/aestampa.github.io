7ee0a51c8863a592cc99a36fc0095c0cac8c23e1
aestampa <abigail.estampador@outlook.com>
2024-06-07 19:00:07 -0600

    Moving index.html to root

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     files: {
-      appTemplate: 'src/index.html' // Add this line
+      appTemplate: 'index.html' // Add this line
     }
   },
 };
